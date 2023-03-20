487649e62990f893a159d0d2c4af679dbcdd97ea
Olga <olkalana@gmail.com>
2023-03-20 19:59:50 +0000

    logical operators - home tasks

--- js/script.js
+++ js/script.js
@@ -1,30 +1,46 @@
 'use strict';
 
+console.log( NaN || 2 || undefined );
+
+console.log( NaN && 2 && undefined );
+
+console.log( 1 && 2 && 3 );
+
+console.log( !1 && 2 || !3 );
+
+console.log( 25 || null && !3 );
+
+console.log( NaN || null || !3 || undefined || 5);
+
+console.log( NaN || null && !3 && undefined || 5);
+
+console.log( 5 === 5 && 3 > 1 || 5);
+
 const hamburger = 3;
 const fries = 3;
 const cola = 0;
 const nuggets = 2;
 
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-    console.log('Ok!');
-} else {
-    console.log('Not today');
+if (hamburger === 3 && cola || fries === 3 && nuggets) {
+   console.log('Done!')
 }
-console.log(hamburger === 3 && cola === 2 || nuggets && fries === 3);
-console.log(hamburger || cola || fries);
 
-console.log(hamburger && cola && fries);
-console.log(1 && 0);
-console.log(1 && 5);
-console.log(null && 5);
-console.log(0 && 'dggrebtrn');
 
-console.log(1 || 0);
-console.log(1 || 5);
-console.log(null || 5);
-console.log(0 || 'dggrebtrn');
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
 
-let alexReport, samReport, kateReport, maryReport = 'done';
-console.log(alexReport || samReport || kateReport || maryReport);
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
+}
 
-console.log(!0);
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
