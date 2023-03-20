3989d979d153a341f3807667de038147233a4200
Olga <olkalana@gmail.com>
2023-03-20 19:07:42 +0000

    practice with conditions

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
 'use strict';
 
-const numberOfFilms = +prompt('How much films you already saw?', '');
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-const movie1 = prompt('What is one of the last movies you saw?', '');
-const rate1 = prompt('How you can rate it?', '');
-const movie2 = prompt('What is one of the last movies you saw?', '');
-const rate2 = prompt('How you can rate it?', '');
+if (1) {
+    console.log('Ok!');
+} else {
+    console.log('Error!');
+}
 
-personalMovieDB.movies[movie1] = rate1;
-personalMovieDB.movies[movie2] = rate2;
+const num = 50;
 
-console.log(personalMovieDB);
+if (num < 49) {
+    console.log('num < 49');
+} else if (num > 100) {
+    console.log('num > 100');
+} else {
+    console.log('num > 49 and num < 100');
+}
+
+const num2 = 51;
+(num2 === 50) ? console.log(true) : console.log(false);
+
+const num3 = 50;
+switch (num3) {
+    case 49:
+        console.log('49');
+        break;
+    case 100:
+        console.log('100');
+        break;
+    case '50':
+        console.log('50');
+        break;
+    default:
+        console.log('default');
+        break;
+}
