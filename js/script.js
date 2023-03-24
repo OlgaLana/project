0eea7c39ed2b48738ccca33eb0d378bda16bcacf
Olga <olkalana@gmail.com>
2023-03-24 18:59:28 +0000

    cycles lesson 2

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 'use strict';
 
-let num =50;
-
-// while (num < 55) {
-//    console.log(num);
-//    num++;
-// }
-
-// do {
-//    console.log(num);
-//    num++;
-// }
-// while (num < 55);
-
-for (let i = 1; i < 10; i++) {
-   if (i === 6) {
-      // break;
-      continue;
+for (let i = 0; i < 3; i++) {
+   console.log('i = ' + i);
+   for (let j = 0; j < 3; j++) {
+      console.log('j = ' + j);
    }
-   console.log(i);
+}
+
+let result = '';
+const length = 9;
+
+for (let i = 1; i < length; i++) {
+   for (let j = 0; j < i; j++) {
+      result += '*';
+   }
+   result += '\n';
+}
+
+console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+   console.log('i = ' + i);
+   for (let j = 0; j < 3; j++) {
+      console.log('j = ' + j);
+      for (let k = 0; k < 3; k++) {
+         if (k === 2) continue first;
+         console.log('k = ' + k);
+      }
+   }
 }
