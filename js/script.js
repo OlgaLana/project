359ab953e16fe68328a6c131cb99553d3e9c6696
Olga <olkalana@gmail.com>
2023-03-25 18:18:52 +0000

    cycles practice 2

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 'use strict';
 
-for (let i = 2; i <= 16; i++) {
-    if (i % 2 === 0) {
-        continue;
-    } else {
-        console.log(i);
-    }
-}
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
 
-let i = 2;
-while (i < 16) {
-    i++;
-    if (i % 2 === 0) {
-        continue;
-    } else {
-        console.log(i);
-    }
-}
-
-const arrayOfNumbers = [];
     // Пишем решение вот тут
-let j = 0;
-for (let i = 5; i <= 10; i++) {
-    arrayOfNumbers[j] = i;
-    j++;
-}
-console.log(arrayOfNumbers);
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+    console.log(result);
+    
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
 
-for (let i = 20; i >= 10; i--) {
-    console.log(i);
-    if (i === 13) break;
-}
+    // Пишем решение вот тут
+    for (let i = 0; i < data.length; i++) {
+        if (typeof(data[i]) === 'string') {
+            data[i] = `${data[i]} - done`;
+        } else {
+            data[i] = data[i] * 2;
+        }
+    }
+    console.log(data);
+    
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    // Пишем решение вот тут
+    let j = 0;
+    for (let i = data.length - 1; i > -1; i--) {
+        result[j] = data[i];
+        j++;
+    }
+    console.log(result);
